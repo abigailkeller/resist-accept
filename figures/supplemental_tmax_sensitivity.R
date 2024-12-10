802533da4f3e84a3e486377e9cb93cd45683be56
Abby <agkeller@berkeley.edu>
2024-12-10 11:10:23 -0800

    tmax sensitivity analysis

---
+++ figures/supplemental_tmax_sensitivity.R
@@ -0,0 +1,167 @@
+##################################
+# Supplemental: Tmax sensitivity #
+##################################
+
+# load packages
+library(tidyverse)
+library(patchwork)
+library(viridis)
+library(ggpubr)
+library(RColorBrewer)
+
+# read in data
+out_df <- readRDS('data/tmax_sensitivity_policies.rds')
+
+# create long df
+out_df_long <- out_df %>% 
+  pivot_longer(cols=!c(penal,loss,Tmax),
+               values_to = 'policy', names_to = 'state')
+
+# convert to numeric
+out_df_long$state <- as.numeric(out_df_long$state)
+out_df_long$policy <- as.numeric(out_df_long$policy)
+
+K <- 100000
+
+# Tmax = 25
+Tmax_25 <- ggplot()+
+  geom_line(data=out_df_long %>%
+              filter(Tmax =='25'),
+            aes(x=state,y=policy,color=penal,linetype=loss),linewidth=1.5)+
+  scale_color_manual(values=c('green4','mediumorchid'),
+                     labels=c('Nonlinear','Linear'))+
+  scale_linetype_manual(values=c('solid','dotted'),
+                        labels=c('Exponential','Sigmoidal'))+
+  scale_y_continuous(limits=c(0,1))+
+  scale_x_continuous(breaks=c(0,0.5*K,K),labels=c(0,'0.5K','K'),
+                     limits=c(0,1.1*K))+
+  labs(x='local EGC density',y='optimal removal effort')+
+  ggtitle(paste0('A. Tmax = 25'))+
+  theme_minimal()+
+  theme(legend.title.align=0.5,
+        legend.position='None',
+        axis.text=element_text(size=9),
+        axis.title=element_text(size=11),
+        plot.title = element_text(size=12))
+
+# Tmax = 50
+Tmax_50 <- ggplot()+
+  geom_line(data=out_df_long %>%
+              filter(Tmax =='50'),
+            aes(x=state,y=policy,color=penal,linetype=loss),linewidth=1.5)+
+  scale_color_manual(values=c('green4','mediumorchid'),
+                     labels=c('Nonlinear','Linear'))+
+  scale_linetype_manual(values=c('solid','dotted'),
+                        labels=c('Exponential','Sigmoidal'))+
+  scale_y_continuous(limits=c(0,1))+
+  scale_x_continuous(breaks=c(0,0.5*K,K),labels=c(0,'0.5K','K'),
+                     limits=c(0,1.1*K))+
+  labs(x='local EGC density',y='optimal removal effort')+
+  ggtitle(paste0('B. Tmax = 50'))+
+  theme_minimal()+
+  theme(legend.title.align=0.5,
+        legend.position='None',
+        axis.text=element_text(size=9),
+        axis.title=element_text(size=11),
+        plot.title = element_text(size=12))
+
+# Tmax = 100
+Tmax_100 <- ggplot()+
+  geom_line(data=out_df_long %>%
+              filter(Tmax =='100'),
+            aes(x=state,y=policy,color=penal,linetype=loss),linewidth=1.5)+
+  scale_color_manual(values=c('green4','mediumorchid'),
+                     labels=c('Nonlinear','Linear'))+
+  scale_linetype_manual(values=c('solid','dotted'),
+                        labels=c('Exponential','Sigmoidal'))+
+  scale_y_continuous(limits=c(0,1))+
+  scale_x_continuous(breaks=c(0,0.5*K,K),labels=c(0,'0.5K','K'),
+                     limits=c(0,1.1*K))+
+  labs(x='local EGC density',y='optimal removal effort')+
+  ggtitle(paste0('C. Tmax = 100'))+
+  theme_minimal()+
+  theme(legend.title.align=0.5,
+        legend.position='None',
+        axis.text=element_text(size=9),
+        axis.title=element_text(size=11),
+        plot.title = element_text(size=12))
+
+# Tmax = 150
+Tmax_150 <- ggplot()+
+  geom_line(data=out_df_long %>%
+              filter(Tmax =='150'),
+            aes(x=state,y=policy,color=penal,linetype=loss),linewidth=1.5)+
+  scale_color_manual(values=c('green4','mediumorchid'),
+                     labels=c('Nonlinear','Linear'))+
+  scale_linetype_manual(values=c('solid','dotted'),
+                        labels=c('Exponential','Sigmoidal'))+
+  scale_y_continuous(limits=c(0,1))+
+  scale_x_continuous(breaks=c(0,0.5*K,K),labels=c(0,'0.5K','K'),
+                     limits=c(0,1.1*K))+
+  labs(x='local EGC density',y='optimal removal effort')+
+  ggtitle(paste0('D. Tmax = 150'))+
+  theme_minimal()+
+  theme(legend.title.align=0.5,
+        legend.position='None',
+        axis.text=element_text(size=9),
+        axis.title=element_text(size=11),
+        plot.title = element_text(size=12))
+
+# Tmax = 200
+Tmax_200 <- ggplot()+
+  geom_line(data=out_df_long %>%
+              filter(Tmax =='200'),
+            aes(x=state,y=policy,color=penal,linetype=loss),linewidth=1.5)+
+  scale_color_manual(values=c('green4','mediumorchid'),
+                     labels=c('Nonlinear','Linear'))+
+  scale_linetype_manual(values=c('solid','dotted'),
+                        labels=c('Exponential','Sigmoidal'))+
+  scale_y_continuous(limits=c(0,1))+
+  scale_x_continuous(breaks=c(0,0.5*K,K),labels=c(0,'0.5K','K'),
+                     limits=c(0,1.1*K))+
+  labs(x='local EGC density',y='optimal removal effort')+
+  ggtitle(paste0('E. Tmax = 200'))+
+  theme_minimal()+
+  theme(legend.title.align=0.5,
+        legend.position='None',
+        axis.text=element_text(size=9),
+        axis.title=element_text(size=11),
+        plot.title = element_text(size=12))
+
+# legend plot
+legend_plot <- ggplot()+
+  geom_line(data=out_df_long %>%
+              filter(Tmax =='200'),
+            aes(x=state,y=policy,color=penal,linetype=loss),linewidth=1.5)+
+  scale_color_manual(values=c('green4','mediumorchid'),
+                     labels=c('Nonlinear','Linear'))+
+  scale_linetype_manual(values=c('solid','dotted'),
+                        labels=c('Exponential','Sigmoidal'))+
+  scale_y_continuous(limits=c(0,1))+
+  scale_x_continuous(breaks=c(0,0.5*K,K),labels=c(0,'0.5K','K'),
+                     limits=c(0,1.1*K))+
+  labs(x='local EGC density',y='',
+       color='Removal cost function',
+       linetype='Ecological change function')+
+  ggtitle(paste0('D. Tmax = 200'))+
+  theme_minimal()+
+  theme(legend.title.align=0.5,
+        axis.text=element_text(size=9),
+        axis.title=element_text(size=11),
+        plot.title = element_text(size=12))
+legend <- get_legend(legend_plot)
+
+
+
+#define plot layout
+layout <- "
+AAABBBEEE
+AAABBBEEE
+CCCDDDFFF
+CCCDDDFFF
+"
+
+finalfigure <- Tmax_25+Tmax_50+Tmax_100+Tmax_150+Tmax_200+legend+plot_layout(design=layout)
+ggsave('figures/supplemental_tmax_sensitivity.tiff',finalfigure,
+       dpi=700,width=6.5,height=4)
+
---
+++ sensitivity_stationary_policies.R
@@ -0,0 +1,134 @@
+################################
+# Creating stationary policies #
+################################
+
+
+# load packages
+library(tidyverse)
+
+# define state space (species density)
+K <- 100000 # carrying capacity
+states <- seq(0,K*1.4,by=1000)
+
+# define action space (units of effort, ranging from 0 to 1)
+actions <- seq(0, 1, 0.01)
+
+# Time horizon
+Tmax <- c(25,50,100,150,200)
+
+# import transition matrix
+transition_filename <- "20240524_TM_4_0.125.rds"
+
+
+# types of ecological change functions (loss types) and removal cost functions (penaltypes)
+losstypes <- c('sig','exp')
+penaltypes <- c('hpenal','lpenal')
+
+# function to get utility matrix filenames
+get_utility_filenames <- function(filename){
+  #split up filename
+  sub1 <- str_split(filename,'_')[[1]]
+  sub2 <- paste0(str_split(sub1[4],'[.]')[[1]][1],'.',
+                 str_split(sub1[4],'[.]')[[1]][2])
+  
+  out <- paste0(sub1[1],"_UM_",sub1[3],
+                  "_",sub2)
+  
+  utility_filenames <- c()
+  
+  for(i in 1:length(losstypes)){
+    for(j in 1:length(penaltypes)){
+      utility_filenames <- append(utility_filenames,paste0(out,"_",penaltypes[j],"_",losstypes[i],".rds"))
+    }
+  }
+  
+  return(utility_filenames)
+}
+
+
+# create empty data frame to store policies
+out_df <- as.data.frame(matrix(NA,nrow=0,ncol=length(states)+3))
+colnames(out_df) <- c('penal','loss','Tmax',states)
+
+# set discount factor
+beta <- 0.99
+
+# loop through transition matrices and utility matrices to get stationary policies through backward iteration
+for(i in 1:length(Tmax)){
+  
+  #load transition matrix
+  transition <- readRDS(paste0('data/transition_matrices/',transition_filename))
+  
+  #get associated utility filenames
+  utility_filenames <- get_utility_filenames(transition_filename)
+  
+  for(j in 1:length(utility_filenames)){
+    
+    #load utility matrix
+    utility <- readRDS(paste0('data/utility_matrices/',utility_filenames[j]))
+    
+    ####
+    #run MDP -- backward iteration
+    ####
+    #mdp_output <- mdp_value_iteration(transition,utility,discount=0.99)
+    
+    # Action value vector at tmax
+    Vtmax <- numeric(length(states))
+    
+    # Action value vector at t and t+1
+    Vt <- numeric(length(states))
+    Vtplus <- numeric(length(states))
+    
+    # Optimal policy vector
+    D <- numeric(length(states))
+    
+    # The backward iteration consists in storing action values in the vector Vt which is the maximum of
+    # utility plus the future action values for all possible next states. Knowing the final action 
+    # values, we can then backwardly reset the next action value Vtplus to the new value Vt. We start 
+    # The backward iteration at time T-1 since we already defined the action #value at Tmax.
+    for (t in (Tmax[i]-1):1) {
+      
+      # We define a matrix Q that stores the updated action values for #all states (rows)
+      # actions (columns)
+      Q <- array(0, dim=c(length(states), length(actions)))
+      
+      for (a in 1:length(actions)) {
+        
+        # For each harvest rate we fill for all states values (row) #the ith column (Action) of matrix Q
+        # The utility of the ith action recorded for all states is #added to the product of the transition 
+        # matrix of the ith #action by the action value of all states 
+        Q[,a] <- utility[,a] + beta*(transition[,,a] %*% Vtplus)
+        
+      } # end of the harvest loop
+      
+      # Find the optimal action value at time t is the maximum of Q
+      Vt <- apply(Q, 1, max)
+      
+      # After filling vector Vt of the action values at all states, we #update the vector Vt+1 to Vt and we go to the next step standing #for previous time t-1, since we iterate backward
+      Vtplus <- Vt
+      
+    } # end of the time loop
+    
+    for (k in seq_along(states)) {
+      # We look for each state which column of Q corresponds to the #maximum of the last updated value 
+      # of Vt (the one at time t+1). If the index vector is longer than 1 #(if there is more than one optimal value we chose the minimum #harvest rate)
+      D[k] <- actions[(min(which(round(Q[k,],13) == round(Vt[k],13))))]
+    }
+    
+    #get metadata from filenames
+    out_penal <- ifelse(length(str_split(utility_filenames[j],'_')[[1]])==6,
+                        str_split(utility_filenames[j],'_')[[1]][5],
+                        str_split(utility_filenames[j],'_')[[1]][6])
+    out_loss <- ifelse(length(str_split(utility_filenames[j],'_')[[1]])==6,
+                       str_split(str_split(utility_filenames[j],'_')[[1]][6],'[.]')[[1]][1],
+                       str_split(str_split(utility_filenames[j],'_')[[1]][7],'[.]')[[1]][1])
+    
+    #concatenate data and add to df
+    out_df[nrow(out_df)+1,] <- c(out_penal,out_loss,Tmax[i],D)
+  }
+}
+
+# save output
+saveRDS(out_df,'data/tmax_sensitivity_policies.rds')
+
+
